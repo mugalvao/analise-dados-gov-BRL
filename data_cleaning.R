ff126f8ec63b1ccbe54372e2d504ce1e8e0ff469
tardelr <renantardelli@gmail.com>
2018-05-13 17:33:36 -0300

    Precisa atualizar para a nova estrutura de pastas

---
+++ data_cleaning.R
@@ -0,0 +1,34 @@
+library(data.table)
+#Cria lista de arquivos .txt na pasta raw_data
+setwd("~/Desktop/tcc/bases/rais/analise_murilo/") # Tem que editar para cada computador que for rodar !!
+
+path_r <- "raw_data/" # Editar aqui para colocar output em outro lugar
+raw_files <- list.files(path_r)
+
+#Local dos arquivos depois de limpar
+path_clean <- "data/" # Editar aqui para colocar output em outro lugar
+
+# Como a ordem das colunas não muda podemos indexar pelo numero da coluna ou renomear 
+for(file in raw_files){
+  start_time <- Sys.time()
+  print(paste("Reading file: ",file,sep = ""))
+  base_full <- fread(paste(path, file, sep=""), sep = ";", showProgress = F)
+  
+  new_cols=c('bairro_sp', 'bairros_fortaleza', 'bairros_rj', 'cnae2', 'cnae95',
+             'distrito_sp', 'qtd_vclt', 'qtd_vativo', 'qtd_vestat', 'atividade_ano', 
+             'cei', 'pat', 'rais_negativa', 'ind_simples', 'municipio',
+             'natureza_jur', 'regiao_df', 'cnae2_sub', 'tamanho_estab', 'tipo_estab',
+             'tipo_estab2')
+
+  # Cria colunas para os arquivos que nao tem, senao da erro porque o new_cols tem que tem mesmo length do dataframe
+  file_cols <- dim(base_full)[2]
+  dif <- file_cols - length(new_cols)
+  if (length(new_cols) < file_cols){
+    base_full[,(length(new_cols)+1):(length(new_cols) + dif)] <- NULL
+  }
+  
+  names(base_full) <- new_cols
+  base_full <- write.csv(base_full[base_full$municipio == 355030,],
+                         file=paste(path_clean, file, ".csv", sep = ""))
+  print(Sys.time()-start_time)
+}
